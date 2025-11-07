50deb9251990d134553788fac6e7bf5ee2779f68
jannick.richter@accenture.com <=>
2025-11-07 09:28:02 +0100

    Update .gitignore to exclude VSCode settings and modify README for project clarity; change prime number generation to 2048 bits in App.java

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ public class App {
 
         Prime prime = new Prime(0.00000000000001);
 
-        System.out.println("Prime: " + prime.getPrime(512));
+        System.out.println("Prime: " + prime.getPrime(2048));
         
         /*
         Scanner scanner = new Scanner(System.in);
